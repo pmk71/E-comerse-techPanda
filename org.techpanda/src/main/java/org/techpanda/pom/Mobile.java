aa96824c8306d24b56787dac92ff98562330ecda
Selenium <sachinbagali1998@gmail.com>
2023-07-24 14:24:21 +0545

    aaa

--- org.techpanda/src/main/java/org/techpanda/pom/Mobile.java
+++ org.techpanda/src/main/java/org/techpanda/pom/Mobile.java
@@ -17,142 +17,142 @@ import org.testng.Reporter;
 
 public class Mobile {
 	FileLib f= new FileLib();
-	
-@FindBy(xpath = "//select[@title='Sort By']")
-private WebElement sortBy;
 
-@FindBy(xpath="//ul[@class]/li/a[@title]")
-private List<WebElement> allItems;
+	@FindBy(xpath = "//select[@title='Sort By']")
+	private WebElement sortBy;
 
-@FindBy(xpath="//a[@title='Sony Xperia']/../..//span[@class='price']")
-private WebElement priceSonyExperia;
+	@FindBy(xpath="//ul[@class]/li/a[@title]")
+	private List<WebElement> allItems;
 
-@FindBy(xpath = "//a[@title='Xperia']")
-private WebElement clickonxperia;
+	@FindBy(xpath="//a[@title='Sony Xperia']/../..//span[@class='price']")
+	private WebElement priceSonyExperia;
 
-@FindBy(xpath="//span[text()='Sony Xperia']/../..//span[@class='price']")
-private WebElement priceSonyExperiafromDetailsPage;
+	@FindBy(xpath = "//a[@title='Xperia']")
+	private WebElement clickonxperia;
 
-
-@FindBy(xpath = "//a[text()='Sony Xperia' and @title]/../..//button")
-private WebElement addSonytoCart;
+	@FindBy(xpath="//span[text()='Sony Xperia']/../..//span[@class='price']")
+	private WebElement priceSonyExperiafromDetailsPage;
 
 
-@FindBy(xpath = "//input[@type='text']")
-private WebElement qtyToSony;
-
-@FindBy(xpath = "//button[@name='update_cart_action']//span[text()='Update']")
-private WebElement addToCartBtnQtyUpdate;
+	@FindBy(xpath = "//a[text()='Sony Xperia' and @title]/../..//button")
+	private WebElement addSonytoCart;
 
 
-@FindBy(xpath = "//p[@class='item-msg error']")
-private WebElement errorMessage;
+	@FindBy(xpath = "//input[@type='text']")
+	private WebElement qtyToSony;
+
+	@FindBy(xpath = "//button[@name='update_cart_action']//span[text()='Update']")
+	private WebElement addToCartBtnQtyUpdate;
+
+
+	@FindBy(xpath = "//p[@class='item-msg error']")
+	private WebElement errorMessage;
 
 
 
-@FindBy(xpath = "//a[@class='skip-link skip-cart ']")
-private WebElement miniCart;
+	@FindBy(xpath = "//a[@class='skip-link skip-cart ']")
+	private WebElement miniCart;
 
-@FindBy(xpath = "//a[@title='Remove This Item']")
-private WebElement removeBtn;
-
-
-@FindBy(xpath = "//p[@class='empty']")
-private WebElement cartMessage;
-
-@FindBy(xpath = "//a[text()='IPhone']/../..//a[text()='Add to Compare']")
-private WebElement addToCompareIphone;
-
-@FindBy(xpath = "//a[text()='Sony Xperia']/../..//a[text()='Add to Compare']")
-private WebElement addToCompareSony;
-
-@FindBy(xpath = "//button/span/span[text()='Compare']")
-private WebElement compareBtn;
-
-@FindBy(xpath = "//h1")
-private WebElement headingOfComapre;
-
-public Mobile(WebDriver driver)
-{
-	PageFactory.initElements(driver, this);
-}
-
-public WebElement getSortBy() {
-	return sortBy;
-}
-
-public List<WebElement> getAllItems() {
-	return allItems;
-}
-
-public String getPriceOfSonyXperia()
-{
-	return priceSonyExperia.getText();
-}
+	@FindBy(xpath = "//a[@title='Remove This Item']")
+	private WebElement removeBtn;
 
 
-public void clickOnxperia()
-{
-	clickonxperia.click();
-}
-public String getPriceOfSonyXperiaFromDetailsPage()
-{
-	return priceSonyExperiafromDetailsPage.getText();
-}
+	@FindBy(xpath = "//p[@class='empty']")
+	private WebElement cartMessage;
 
-public void addMoreProductsToCart()
-{
-	addSonytoCart.click();
-	qtyToSony.sendKeys("1000");
-	addToCartBtnQtyUpdate.click();
-	
-}
+	@FindBy(xpath = "//a[text()='IPhone']/../..//a[text()='Add to Compare']")
+	private WebElement addToCompareIphone;
 
-public WebElement getErrorMessage() {
-	return errorMessage;
-}
+	@FindBy(xpath = "//a[text()='Sony Xperia']/../..//a[text()='Add to Compare']")
+	private WebElement addToCompareSony;
 
-public void clearCart(WebDriver driver)
-{
-	miniCart.click();
-	removeBtn.click();
-	driver.switchTo().alert().accept();
-	String text = cartMessage.getText();
-	Reporter.log(text);
-}
+	@FindBy(xpath = "//button/span/span[text()='Compare']")
+	private WebElement compareBtn;
 
-public void compareMobile(WebDriver driver) throws EncryptedDocumentException, IOException
-{
-	addToCompareIphone.click();
-	addToCompareSony.click();
-	compareBtn.click();
- Set<String> win = driver.getWindowHandles();
- 
- boolean count = (win.size()>1);
- assertTrue(count,"the window is not opened in separate window");
- String atext = f.getExcelData("test4", 0, 1);
- 
+	@FindBy(xpath = "//h1")
+	private WebElement headingOfComapre;
 
- for(String w:win)
- {
-	 driver.switchTo().window(w).getTitle();
-	 if(headingOfComapre.equals(atext))
-	 {
-		 //verifying the heading 
-		 assertEquals(atext, headingOfComapre.getText(),"heading is not matching");
-		 
-		 // closing the window
-		 driver.close();
-		 
-		 //verifying pop-up window is closed or not
-		 
-		Set<String> count2 = driver.getWindowHandles();
-		boolean condition2 = (count2.size()==1);
-		assertTrue(condition2);
-	
-		
+	public Mobile(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
 	}
- }
-}
+
+	public WebElement getSortBy() {
+		return sortBy;
+	}
+
+	public List<WebElement> getAllItems() {
+		return allItems;
+	}
+
+	public String getPriceOfSonyXperia()
+	{
+		return priceSonyExperia.getText();
+	}
+
+
+	public void clickOnxperia()
+	{
+		clickonxperia.click();
+	}
+	public String getPriceOfSonyXperiaFromDetailsPage()
+	{
+		return priceSonyExperiafromDetailsPage.getText();
+	}
+
+	public void addMoreProductsToCart()
+	{
+		addSonytoCart.click();
+		qtyToSony.sendKeys("1000");
+		addToCartBtnQtyUpdate.click();
+
+	}
+
+	public WebElement getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void clearCart(WebDriver driver)
+	{
+		miniCart.click();
+		removeBtn.click();
+		driver.switchTo().alert().accept();
+		String text = cartMessage.getText();
+		Reporter.log(text);
+	}
+
+	public void compareMobile(WebDriver driver) throws EncryptedDocumentException, IOException
+	{
+		addToCompareIphone.click();
+		addToCompareSony.click();
+		compareBtn.click();
+		Set<String> win = driver.getWindowHandles();
+
+		boolean count = (win.size()>1);
+		assertTrue(count,"the window is not opened in separate window");
+		String atext = f.getExcelData("test4", 0, 1);
+
+
+		for(String w:win)
+		{
+			driver.switchTo().window(w).getTitle();
+			if(headingOfComapre.equals(atext))
+			{
+				//verifying the heading 
+				assertEquals(atext, headingOfComapre.getText(),"heading is not matching");
+
+				// closing the window
+				driver.close();
+
+				//verifying pop-up window is closed or not
+
+				Set<String> count2 = driver.getWindowHandles();
+				boolean condition2 = (count2.size()==1);
+				assertTrue(condition2);
+
+
+			}
+		}
+	}
 
 }
